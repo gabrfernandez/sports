bd76121e65ee12ef5f3742384853aaf139b6f8ec
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2021-03-12 21:36:13 -0800

    restful api endpoints

--- server/controllers/athlete.controller.js
+++ server/controllers/athlete.controller.js
@@ -1,4 +1,5 @@
 const {Athlete} = require('../models/athlete.model'); 
+const athleteRoutes = require('../routes/athlete.routes');
 
 module.exports.list=(request, response)=>{
     Athlete.find({})
@@ -7,5 +8,64 @@ module.exports.list=(request, response)=>{
         })
         .catch(err=>{
             response.status(400).json(err);
+        })
+}
+
+module.exports.create=(request, response)=>{
+    const {firstName, lastName, sport, team} = request.body;
+    Athlete.create({
+        firstName,
+        lastName,
+        sport,
+        team
+    })
+        .then(athlete=>{
+            response.json(athlete)
+        })
+        .catch(err=>{
+            response.status(400).json(err)
+        })
+}
+
+module.exports.detail=(request, response)=>{
+    const {id} =reques.params;
+    Athlete.findOne({_id: id})
+        .then(athlete=>{
+            response.json(athlete)
+        })
+        .catch(err=>{
+            response.status(400).json(err);
+        })
+}
+
+module.exports.update =(request, response)=>{
+    const {id} = request.params;
+    const {firstName, lastName, sport, team}=request.body;
+    Athlete.findOneAndUpdate({_id: id}, {
+        firstName, 
+        lastName, 
+        sport, 
+        team
+    },
+        {
+            new:true,
+            useFindAndModify:true
+        })
+        .then(athlete=>{
+            response.json(athlete)
+        })
+        .catch(err=>{
+            response.status(400).json(err)
+        })
+}
+
+module.exports.delete=(request, response)=>{
+    const {id}=request.params;
+    Athlete.deleteOne({_id: id})
+        .then(deleteConfirmation=>{
+            response.json(deleteConfirmation);
+        })
+        .catch(err=>{
+            response.status(400).json(err)
         })
 }
